0f9331710e007b91819058bc82715c65dcef4f92
jjmaldonis <jjmaldonis@gmail.com>
2016-05-18 22:00:44 -0500

    added a setup.py file

---
+++ setup.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python
+
+import sys
+
+from setuptools import setup, find_packages
+
+
+install_requires = [
+    "ase",
+    "numpy",
+    "scipy",
+    "mpi4py"
+]
+
+setup(
+    name="StructOpt",
+    version="0.5",
+    author="UW Madison, Materials Science",
+    author_email="maldonis@wisc.edu",
+    url="https://github.com/uw-cmg/StructOpt_modular",
+    description="Atomic Stucture Optimization Framework",
+    keywords=["Atomic", "Stucture", "Optimization", "Materials Science"],
+    classifiers=[
+        "Development Status :: 4 - Beta",
+        "Programming Language :: Python :: 3",
+        "Environment :: Web Environment",
+        "Operating System :: Linux",
+        "Intended Audience :: Developers",
+    ],
+    packages=find_packages(),
+    zip_safe=True,
+    install_requires=install_requires
+)
