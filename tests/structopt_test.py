0b627508cb66b121f166cdee579f9f48f0df52b5
Zhongnan Xu <zhongnan.xu@gmail.com>
2016-07-11 15:03:35 -0500

    Removed tests (kept local version for myself)

--- tests/structopt_test.py
+++
@@ -1,9 +0,0 @@
-
-import sys; sys.path.insert(1, '../')
-import structopt
-
-print(structopt.__file__)
-sys.exit()
-
-# structopt.setup('sample-inputs/Au309/structopt.in.json')
-structopt.setup('sample-inputs/Au18/structopt.in.json')
