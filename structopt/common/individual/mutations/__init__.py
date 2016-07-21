57127d7ea4bb5b505e813dcd545fd733a267649d
Zhongnan Xu <zhongnan.xu@gmail.com>
2016-07-21 16:16:35 -0500

    Changed mutation parameter format

--- structopt/common/individual/mutations/__init__.py
+++ structopt/common/individual/mutations/__init__.py
@@ -21,10 +21,9 @@ class Mutations(object):
     def __init__(self, parameters):
         # These variables never change
         self.parameters = parameters
-        self.kwargs = defaultdict(dict)
-        self.kwargs.update( {getattr(self, name): kwords for name, kwords in self.parameters.kwargs.items()} )
 
-        self.mutations = {getattr(self, name): prob for name, prob in self.parameters.options.items()}
+        self.mutations = {getattr(self, mut): self.parameters[mut]['probability'] for mut in self.parameters}
+        self.kwargs = {mut: self.parameters[mut]['kwargs'] for mut in self.parameters}
         total_probability = sum(self.mutations.values())
         self.mutations[None] = 1.0 - total_probability
 
@@ -49,7 +48,8 @@ class Mutations(object):
         else:
             individual._relaxed = False
             individual._fitted = False
-            return self.selected_mutation(individual, **self.kwargs[self.selected_mutation])
+            kwargs = self.kwargs[self.selected_mutation.__name__.split('.')[-1]]
+            return self.selected_mutation(individual, **kwargs)
 
     @single_core
     def post_processing(self):
