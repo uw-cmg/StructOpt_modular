526e3abf4f792409f261b605b99d464c35682924
Zhongnan Xu <zhongnan.xu@gmail.com>
2016-07-25 11:22:09 -0500

    Picked only population // 2 parents from combinations

--- structopt/common/population/selections/random_selection.py
+++ structopt/common/population/selections/random_selection.py
@@ -1,6 +1,9 @@
 from itertools import combinations
+import random
 
 def random_selection(population, fits):
     pairs = [pair for pair in combinations(population, 2)]
+    random.shuffle(pairs)
+    pairs = pairs[:len(population) // 2]
     return pairs
 
