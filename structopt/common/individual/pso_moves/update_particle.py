1d25f2637521710988a43efd2761cf71462f6235
ZhewenSong <zsong39@wisc.edu>
2016-07-26 19:21:26 -0500

    Fixed PSO serial runs

--- structopt/common/individual/pso_moves/update_particle.py
+++ structopt/common/individual/pso_moves/update_particle.py
@@ -1,18 +1,19 @@
 import sys
 import random
 import logging
-
+import numpy
 import structopt
 from structopt.common.population import Population
 
 
 def update_particle(individual, best_swarm, best_particle, omega, phi_p, phi_g):
     natoms = len(individual.positions)
-    
-    individual.velocities = (omega * individual.velocities +
+    rp = numpy.random.rand(natoms,3)
+    rg = numpy.random.rand(natoms,3)
+    individual.set_velocities(omega * individual.velocities +
                             phi_p*rp*(best_particle.positions - individual.positions)  +
                             phi_g*rg*(best_swarm.positions - individual.positions))
-    individual.positions += individual.velocities
+    individual.set_positions(individual.positions + individual.velocities)
 
     return None
 
