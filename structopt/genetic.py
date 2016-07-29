d561b32e202b779d6d74887c45cc4ea5f4dc08bb
Zhongnan Xu <zhongnan.xu@gmail.com>
2016-07-29 16:25:13 -0500

    Added module to be printed to fitness.log

--- structopt/genetic.py
+++ structopt/genetic.py
@@ -62,7 +62,10 @@ class GeneticAlgorithm(object):
         # Save the fitnesses for each individual
         fitness_logger = logging.getLogger('fitness')
         for individual in self.population:
-            fitness_logger.info('Generation {}, Individual {}: {}'.format(self.generation, individual.index, ', '.join([str(x) for x in individual.fits])))
+            line = 'Generation {}, Individual {}:'.format(self.generation, individual.index)
+            for module in individual.fits:
+                line += ' {}: {}'.format(module, individual.fits[module])
+            fitness_logger.info(line)
 
         # Save the XYZ file for each individual
         for individual in self.population:
