4e341632c3d127ed8bbe56b9a0d2067097fe10bc
Zhongnan Xu <zhongnan.xu@gmail.com>
2016-08-12 13:32:20 -0500

    Added cluster specific mutations

---
+++ structopt/cluster/individual/mutations/__init__.py
@@ -0,0 +1,28 @@
+import functools
+import structopt.common.individual.mutations
+
+from .move_atoms import move_atoms
+from .move_surface_atoms import move_surface_atoms
+from .move_atoms_group import move_atoms_group
+
+move_surface_atoms.tag = 'MoSuAt'
+move_atoms.tag = 'MoAt'
+move_atoms_group.tag = 'MoAtGr'
+
+class Mutations(structopt.common.individual.mutations.Mutations):
+
+    @staticmethod
+    @functools.wraps(move_surface_atoms)
+    def move_surface_atoms(individual, max_natoms=0.2, move_CN=9, surf_CN=11):
+        return move_surface_atoms(individual, max_natoms, move_CN, surf_CN)
+    
+    @staticmethod
+    @functools.wraps(move_atoms)
+    def move_atoms(individual, max_natoms=0.20):
+        return move_atoms(individual, max_natoms=0.20)
+
+    @staticmethod
+    @functools.wraps(move_atoms_group)
+    def move_atoms_group(individual, max_natoms=0.20):
+        return move_atoms_group(individual, max_natoms=0.20)
+    
