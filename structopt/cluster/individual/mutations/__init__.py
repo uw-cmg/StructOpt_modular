acc0090af82ec0922b70420d60202494fb0f91b7
Zhongnan Xu <zhongnan.xu@gmail.com>
2016-08-17 14:17:20 -0500

    Added cluster version of rotate_cluster

--- structopt/cluster/individual/mutations/__init__.py
+++ structopt/cluster/individual/mutations/__init__.py
@@ -4,10 +4,12 @@ import structopt.common.individual.mutations
 from .move_atoms import move_atoms
 from .move_surface_atoms import move_surface_atoms
 from .move_atoms_group import move_atoms_group
+from .rotate_cluster import rotate_cluster
 
 move_surface_atoms.tag = 'MoSuAt'
 move_atoms.tag = 'MoAt'
 move_atoms_group.tag = 'MoAtGr'
+rotate_cluster.tag = 'RoCl'
 
 class Mutations(structopt.common.individual.mutations.Mutations):
 
@@ -25,3 +27,8 @@ class Mutations(structopt.common.individual.mutations.Mutations):
     @functools.wraps(move_atoms_group)
     def move_atoms_group(individual, max_natoms=0.20):
         return move_atoms_group(individual, max_natoms=0.20)
+
+    @staticmethod
+    @functools.wraps(rotate_cluster)
+    def rotate_cluster(individual, max_natoms=0.20):
+        return rotate_cluster(individual, max_natoms=0.20)
---
+++ structopt/cluster/individual/mutations/rotate_cluster.py
@@ -0,0 +1,57 @@
+import random
+import numpy as np
+from ase import Atom, Atoms
+
+from structopt.tools import NeighborList
+
+def rotate_cluster(individual, max_natoms=0.20):
+    """Randomly moves atoms within a cluster.
+    
+    Args:
+        individual (Individual): an individual
+        max_natoms (float or int): if float, the maximum number of atoms that will be moved is max_natoms*len(individual)
+                                   if int, the maximum number of atoms that will be moved is max_natoms
+                                   default: 0.20
+    """
+
+    if not len(individual):
+        return None
+
+    if isinstance(max_natoms, float):
+        assert max_natoms <= 1
+        max_natoms = int(len(individual)*max_natoms)
+
+    NNs = NeighborList(individual)
+    CNs = [len(NN) for NN in NNs]
+    
+    # Get the surface atoms. These provide bounds for the moves
+    # First get unit vectors and mags of all surface atoms
+    positions = individual.get_positions()
+    com = np.sum(positions.T, axis=1) / len(individual)
+    surf_indices = [i for i, CN in enumerate(CNs) if CN < 11]
+    surf_positions = np.array([positions[i] for i in surf_indices])
+    surf_magnitudes = np.linalg.norm(surf_positions - com, axis=1)
+    surf_vectors = (surf_positions - com) / np.array([surf_magnitudes]).T
+
+    # Weight probability of choosing a vector by its length from the center
+    surf_probabilities = surf_magnitudes / sum(surf_magnitudes)
+
+    # Choose a random point inside the particle and find nearest neighbors to that point
+    i = np.random.choice(list(range(len(surf_vectors))), p=surf_probabilities)
+    point = com + surf_vectors[i] * surf_magnitudes[i] * random.random()
+    atom = Atom('Si', point)
+    nearest_indices = individual.get_nearest_atom_indices(atom_index=atom.index, count=max_natoms)
+
+    # Extract out the atoms to be rotated. Popping changes the indices
+    # so pop them out highest to lowest indice
+    nearest_indices = np.sort(nearest_indices)[::-1]
+    atoms = Atoms()
+    for ind in nearest_indices:
+        atoms.append(individual.pop(ind))
+
+    axis = random.choice(['x', '-x', 'y', '-y', 'z', '-z'])
+    angle = random.uniform(30, 180)
+    atoms.rotate(axis, a=angle, center='COM', rotate_cell=False)
+    individual.extend(atoms)
+
+    return None
