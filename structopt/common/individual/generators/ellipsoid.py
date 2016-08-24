91dd4b14117f98b26bdf64335a11c70d709110cc
Zhongnan Xu <zhongnan.xu@gmail.com>
2016-08-24 12:43:47 -0500

    Added a generate ellipsoid function

---
+++ structopt/common/individual/generators/ellipsoid.py
@@ -0,0 +1,38 @@
+import random
+from ase import Atom, Atoms
+
+from structopt.tools import get_particle_radius
+
+def ellipsoid(atomlist, fill_factor=0.74, radii=None, ratio=[1, 1, 1], cell=None):
+    """Generates a random ellipsoid by rejection sampling. Not the most efficient
+    code but good at garaunteeing an even distribution inside the ellipsoid"""
+    
+
+    # Get the dimensions of the ellipsoid if required
+    if size is None:
+        radius = get_particle_radius(atomlist, fill_factor)
+        a = radius / ((ratio[1]/ratio[0])**(1.0/3.0) * (ratio[2]/ratio[0])**(1.0/3.0))
+        b = radius / ((ratio[0]/ratio[1])**(1.0/3.0) * (ratio[2]/ratio[1])**(1.0/3.0))
+        c = radius / ((ratio[0]/ratio[2])**(1.0/3.0) * (ratio[1]/ratio[2])**(1.0/3.0))
+    else:
+        a, b, c = radii
+
+    # Create a list of random order of the atoms
+    chemical_symbols = []
+    for atom in atomlist:
+        chemical_symbols += [atom[0]] * atom[1]
+    random.shuffle(chemical_symbols)
+
+    # Add atoms iteratively only if they fall inside the ellipsoid
+    indiv = Atoms()
+    while len(chemical_symbols) > 0:
+        x, y, z = random.uniform(-a, a), random.uniform(-b, b), random.uniform(-c, c)
+        if x**2/a**2 + y**2/b**2 + z**2/c**2 <= 1:
+            indiv.extend(Atom(chemical_symbols.pop(), [x, y, z]))
+
+    if cell is not None:
+        indiv.set_cell(cell)
+        indiv.center()
+        indiv.set_pbc(True)
+
+    return indiv
