f37a4331f72d8cd7367a798ee250b637e3c61f4e
Zhongnan Xu <zhongnan.xu@gmail.com>
2016-09-06 14:33:11 -0500

    Added permutation mutation

---
+++ structopt/common/individual/mutations/permutation.py
@@ -0,0 +1,18 @@
+import random
+import numpy
+
+def permutation(individual):
+    """Move function to perform Permutation of one atom based on atomlist
+    Inputs:
+        indiv = Individual class object to be altered
+        Optimizer = Optimizer class object with needed parameters
+    Outputs:
+        indiv = Altered Individual class object
+    """
+
+    a1 = individual[random.randint(0, indiv[0].get_number_of_atoms() - 1)]
+    opts = [i for i in individual if i.symbol != a1.symbol]
+    a2 = opts[random.randint(0,len(opts) - 1)]
+    a1.symbol, a2.symbol = a2.symbol, a1.symbol
+
+    return
