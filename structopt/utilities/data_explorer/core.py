641c99d42a1dc3276280eb5c500a5b2a742a698d
jjmaldonis <jjmaldonis@gmail.com>
2016-12-15 02:03:16 -0600

    updated the data explorer and added another example

--- structopt/utilities/data_explorer/core.py
+++ structopt/utilities/data_explorer/core.py
@@ -3,7 +3,11 @@ import sys
 import json
 import re
 import importlib
+import weakref
 from collections import defaultdict, Counter
+import numpy as np
+
+from common import lazy, lazyproperty
 
 from structopt.io import read_xyz
 from structopt.tools.dictionaryobject import DictionaryObject
@@ -20,9 +24,16 @@ class DataExplorer(object):
         self.genealogy_file = os.path.join(dir, 'genealogy.log')
         self.fitnesses_file = os.path.join(dir, 'fitnesses.log')
         self.XYZs = os.path.join(dir, 'XYZs')
-
-        # Load input parameters from output file
         self.output_file = os.path.join(dir, 'output.log')
+        self._icache = {}
+        self._created_killed = None
+
+    @lazyproperty
+    def generations(self):
+        return Generations(self.genealogy_file, self)
+
+    @lazyproperty
+    def parameters(self):
         parameters = []
         start_flag = False
         for line in open(self.output_file):
@@ -37,63 +48,207 @@ class DataExplorer(object):
                 start_flag = False
                 break
         parameters = ''.join(parameters)
-        self.parameters = DictionaryObject(json.loads(parameters))
-        sys.modules['gparameters'] = self.parameters
+        parameters = DictionaryObject(json.loads(parameters))
+        sys.modules['gparameters'] = parameters
+        return parameters
 
-        self.generations = Generations(self.genealogy_file)
-        for population in self.generations:
-            for individual in population.values():
-                individual.filename = os.path.join(self.XYZs, 'generation{}'.format(individual.created_on), 'individual{}.xyz'.format(individual.id))
-                individual.parameters = DictionaryObject(self.parameters.copy())
+    def __getitem__(self, index):
+        return self.generations[index]
+
+    def __len__(self):
+        return len(self.generations)
+
+    def get_historical_individual(self, id):
+        try:
+            history = self._icache[id]
+        except KeyError:
+            history = IndividualHistory(id, self)
+            self._icache[id] = history
+        return history
+
+    def _get_historical_info(self, id):
+        if self._created_killed is None:
+            #print("Parsing Genealogy file...")
+            created_killed = defaultdict(lambda: [np.inf, -np.inf])
+            for generation, population in enumerate(self):
+                #sys.stdout.write("\r%d%%" % generation)
+                #sys.stdout.flush()
+                for id_ in population.keys():
+                    if generation < created_killed[id_][0]:
+                        created_killed[id_][0] = generation
+                    if generation > created_killed[id_][1]:
+                        created_killed[id_][1] = generation
+            self._created_killed = created_killed
+            #print('')
+        return self._created_killed[id]
 
 
-class Generations(list):
-    def __init__(self, genealogy_filename):
+class Generations(object):
+    def __init__(self, genealogy_filename, dataexplorer):
+        self._dataexplorer = weakref.ref(dataexplorer)
         generations = open(genealogy_filename).readlines()
-        generations = [line.strip().split(' : INFO : ')[-1] for line in generations]
-        super().__init__(line.split() for line in generations)
-        individuals = defaultdict(bool)
-        for i, population in enumerate(self):
-            for j, gene in enumerate(population):
-                id, crossover, mutation = parse_gene(gene)
-                if mutation is None:
-                    individuals[id] = individuals[id] or Individual(id=id, created_on=i, mutation_tag=mutation, crossover_tag=crossover)
-                else:
-                    individuals[id] = Individual(id=id, created_on=i, mutation_tag=mutation, crossover_tag=crossover)
-                population[j] = individuals[id]
-            self[i] = Population(self[i])
-            for individual in list(individuals.keys()):
-                if individual not in self[i]:
-                    individuals[individual].killed_on = i
-                    individuals.pop(individual)
-        for individual in individuals:
-            individuals[individual].killed_on = len(self)
+        self._data = [line.strip().split(' : INFO : ')[-1] for line in generations]
+
+    def __getitem__(self, index):
+        if isinstance(self._data[index], Population):
+            return self._data[index]
+        else:
+            self._data[index] = Population(index, self._data[index], self._dataexplorer())
+            return self._data[index]
+
+    def __len__(self):
+        return len(self._data)
 
 
 class Population(dict):
-    def __init__(self, individuals):
-        super().__init__([(individual.id, individual) for individual in individuals])
+    def __init__(self, generation, individuals_from_genealogy_file, dataexplorer):
+        self._generation = generation
+        self._dataexplorer = weakref.ref(dataexplorer)
+        self._data = individuals_from_genealogy_file
+        self._loaded = False
+
+    @property
+    def generation(self):
+        return self._generation
+
+    def __hash__(self):
+        return self.generation
+
+    def _load(self):
+        self._data = self._data.split()
+        self._data = [parse_gene(ind) for ind in self._data]
+        self._data = {id: (c, m) for id, c, m in self._data}
+        super().__init__({id: None for id in self._data if id not in self})
+        self._loaded = True
+
+    def __getitem__(self, key):
+        if key not in self or super().__getitem__(key) is None:
+            if not self._loaded:
+                self._load()
+                if key not in self._data:
+                    raise KeyError(key)
+            history = self._dataexplorer().get_historical_individual(key)
+            ct, mt = self._data[key]
+            self[key] = Individual(key, self.generation, history, self._dataexplorer(), ct, mt)
+        return super().__getitem__(key)
+
+    def __len__(self):
+        return len(self._data)
+
+    def keys(self):
+        if not self._loaded:
+            self._load()
+        return super().keys()
+
+    def items(self):
+        if not self._loaded:
+            self._load()
+        for key in self:
+            self[key]
+        return super().items()
+
+    def values(self):
+        if not self._loaded:
+            self._load()
+        for key in self:
+            self[key]
+        return super().values()
+
+    def __iter__(self):
+        if not self._loaded:
+            self._load()
+        return super().__iter__()
+
+
+class IndividualHistory(dict):
+    def __init__(self, id, dataexplorer):
+        self._id = id
+        self._dataexplorer = weakref.ref(dataexplorer)
+        self._created_on = None
+        self._killed_on = None
+        self._loaded = False
+
+    def __repr__(self):
+        return "<IndividualHistory {}>".format(self.id)
+    __str__ = __repr__
+
+    @property
+    def id(self):
+        return self._id
+
+    def __hash__(self):
+        return self.id
+
+    def _load(self):
+        super().__init__({generation: None for generation in range(self.created_on, self.killed_on+1)})
+
+    def __getitem__(self, generation):
+        return self._dataexplorer()[generation][self.id]
+
+    @property
+    def created_on(self):
+        if self._created_on is not None:
+            return self._created_on
+        c, k = self._dataexplorer()._get_historical_info(self.id)
+        self._created_on = c
+        self._killed_on = k
+        return self._created_on
+
+    @property
+    def killed_on(self):
+        if self._killed_on is not None:
+            return self._killed_on
+        c, k = self._dataexplorer()._get_historical_info(self.id)
+        self._created_on = c
+        self._killed_on = k
+        return self._killed_on
+
+    def __contains__(self, item):
+        if not self._loaded:
+            self._load()
+        return super().__contains__(item)
+
+    def __iter__(self):
+        if not self._loaded:
+            self._load()
+        return super().__iter__()
 
 
 class Individual(object):
-    def __init__(self, id, created_on, killed_on=None, mutation_tag=None, crossover_tag=None, filename=None, parameters=None):
-        self.id = id
-        self.created_on = created_on
-        self.killed_on = killed_on
-        self.filename = filename
+    def __init__(self, id, generation, history, dataexplorer, crossover_tag=None, mutation_tag=None):
+        self._id = id
+        self._history = history
+        self._dataexplorer = weakref.ref(dataexplorer)
+        self._generation = generation
+        self._loaded = False
+
         self.mutation_tag = mutation_tag
         self.crossover_tag = crossover_tag
         if self.mutation_tag is not None:
-            self.mutation_tag = self.mutation_tag[1:]
+            self.mutation_tag = self.mutation_tag
         if self.crossover_tag is not None:
             parents = re.compile('\((\d+)\+(\d+)\)(\w+)')
             parent1, parent2, tag = parents.findall(self.crossover_tag)[0]
             self.parent1 = int(parent1)
             self.parent2 = int(parent2)
             self.crossover_tag = tag
-        self._loaded = False
+
+    def __hash__(self):
+        return (self.id, self.generation)
+
+    @property
+    def id(self):
+        return self._id
+    @property
+    def generation(self):
+        return self._generation
+
+    @property
+    def history(self):
+        return self._history
 
     def __repr__(self):
+        return "<Individual {} @{}>".format(self.id, self.generation)
         if self.mutation_tag is None:
             mtag = ''
         else:
@@ -107,17 +262,19 @@ class Individual(object):
     __str__ = __repr__
 
     def load_structure(self):
-        if "generators" in self.parameters:
-            self.parameters.pop("generators")
-        self.structure_type = self.parameters.structure_type.lower()
+        parameters = self._dataexplorer().parameters
+        if "generators" in parameters:
+            parameters.pop("generators")
+        self.structure_type = parameters.structure_type.lower()
         module = importlib.import_module('structopt.{}'.format(self.structure_type))
         Structure = getattr(module, self.structure_type.title())
-        generator_parameters = {"read_xyz": {"filename": self.filename}}
+        filename = os.path.join(self._dataexplorer().XYZs, 'generation{}'.format(self.generation), 'individual{}.xyz'.format(self.id))
+        generator_parameters = {"read_xyz": {"filename": filename}}
         self._structure = Structure(id=self.id,
-                              relaxation_parameters=self.parameters.relaxations,
-                              fitness_parameters=self.parameters.fitnesses,
-                              mutation_parameters=self.parameters.mutations,
-                              pso_moves_parameters=self.parameters.pso_moves,
+                              relaxation_parameters=parameters.relaxations,
+                              fitness_parameters=parameters.fitnesses,
+                              mutation_parameters=parameters.mutations,
+                              pso_moves_parameters=parameters.pso_moves,
                               generator_parameters=generator_parameters)
         self._loaded = True
         return self._structure
@@ -127,27 +284,26 @@ class Individual(object):
             raise AttributeError("'{}' has no attribute '{}'".format(self.__class__.__name__, key))
         return getattr(self._structure, key)
 
-    def get_parents(self, generations):
+    def get_parents(self, dataexplorer):
         mparent = None
         c1parent = None
         c2parent = None
         if self.crossover_tag is not None:
-            c1parent = generations[self.created_on-1][self.parent1]
-            c2parent = generations[self.created_on-1][self.parent2]
+            c1parent = dataexplorer[self.generation-1][self.parent1]
+            c2parent = dataexplorer[self.generation-1][self.parent2]
         elif self.mutation_tag is not None:
-                mparent = generations[self.created_on-1][self.id]
+                mparent = dataexplorer[self.generation-1][self.id]
         return mparent, c1parent, c2parent
 
-
-    def history(self, generations):
-        if not hasattr(self, '_history'):
+    def full_history(self, generations):
+        if not hasattr(self, '_full_history'):
             history = []
             history.append((self.created_on, self))
             for parent in self.get_parents(generations):
                 if parent is None: continue
                 history.extend(parent.history(generations))
-            self._history = history
-        return self._history
+            self._full_history = history
+        return self._full_history
         #history = defaultdict(list)
         #history[self.created_on].append(self)
         #for parent in self.get_parents(generations):
@@ -155,18 +311,16 @@ class Individual(object):
         #    history[parent.created_on].append(parent.history(generations))
         #return dict(history)
 
-
     def print_history(self, generations):
-        history = self.history(generations)
+        history = self.full_history(generations)
         dhist = defaultdict(list)
         for gen, ind in history:
             dhist[gen].append(ind)
         for gen, hist in sorted(dhist.items()):
             print('{gen}: {hist}'.format(gen=gen, hist=', '.join(str(i) for i in set(hist))))
 
-
     def count_modifiers(self, generations):
-        return self._count_modifiers(self.history(generations))
+        return self._count_modifiers(self.full_history(generations))
 
     @staticmethod
     def _count_modifiers(history):
