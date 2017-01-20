884c8f142858e7f35349479f7ee41e0a82b8de2e
Zhongnan Xu <zhongnan.xu@gmail.com>
2017-01-20 17:01:16 -0600

    First update of data_explorer PDF

--- structopt/utilities/data_explorer/core.py
+++ structopt/utilities/data_explorer/core.py
@@ -57,7 +57,7 @@ class DataExplorer(object):
         fitnesses = {}
         for line in open(self.fitnesses_file):
             firsthalf = re.compile(".+ : INFO : Generation (\d+), Individual (\d+):(.*)")
-            secondhalf = re.compile("([\w]+): (\d+.\d+)")
+            secondhalf = re.compile("([\w]+): ([-\d]+.\d+)")
             generation, id, fitness_str = firsthalf.findall(line)[0]
             fitnesses[(int(generation), int(id))] = {fit: float(value) for fit, value in secondhalf.findall(fitness_str)}
         return fitnesses
