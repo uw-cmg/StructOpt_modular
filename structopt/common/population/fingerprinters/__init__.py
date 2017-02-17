c293c081ee73eb5cb92a6fff68bdad154588a3f7
jjmaldonis <jjmaldonis@gmail.com>
2017-02-17 14:10:21 -0600

    reworked the fingerprinter functionality due to the parallelism

--- structopt/common/population/fingerprinters/__init__.py
+++ structopt/common/population/fingerprinters/__init__.py
@@ -38,72 +38,77 @@ class Fingerprinters(object):
             return
 
         # Apply the selected fingerprinter to all pairs of individuals
-        equivalent_pairs = self.get_equivalent_pairs(population)
-        
+        # The below function parallelizes the calculations, but returns the gathered results
+        # So at this point, all cores are working with the same data and will run the below code
+        # identically -- except for the random numbers!
+        kwargs = self.function_kwargs[self.selected_fingerprinter]
+        equivalent_pairs = self.get_equivalent_pairs(population, self.selected_fingerprinter, kwargs)
+
         if equivalent_pairs and gparameters.mpi.rank == 0:
             if keep_best:
-                fits = [(individual.fitness, individual.id) for individual in population]
-                best = sorted(fits)[0][1]
+                best = sorted(population, key=lambda individual: individual.fitness)[0].id
 
             ids = [i.id for i in population]
             equivalent_pairs = [(a.id, b.id) for a, b in equivalent_pairs]
-            equivalent_individuals = disjoint_set_merge(ids, equivalent_pairs)
-            to_keep = set()
+            # disjoint_set_merge will incldue all ids in `ids` as separate entities even if an id is not in any of `equivalent_pairs`
+            equivalent_sets = disjoint_set_merge(ids, equivalent_pairs)
             killed = set()
-            for equivalent in equivalent_individuals:
-                if keep_best and best in equivalent:
-                    to_keep.add(best)
-                    for x in equivalent:
+            for equivalent_individuals in equivalent_sets:
+                if len(equivalent_individuals) < 2:
+                    continue
+                if keep_best and best in equivalent_individuals:
+                    for x in equivalent_individuals:
                         if x is not best:
                             killed.add(x)
                 else:
-                    equivalent = sorted(equivalent, key=lambda id: population[id].fitness)
-                    to_keep.add(equivalent[0])
-                    for x in equivalent[1:]:
+                    equivalent_individuals = sorted(equivalent_individuals, key=lambda id: population[id].fitness)
+                    for x in equivalent_individuals[1:]:
                         killed.add(x)
 
-            while len(to_keep) < nkeep:
-                to_keep.add(
-                    random.choice(
-                        [id for id in ids if id not in to_keep]
-                    )
-                )
+            check = False
+            while len(population) - len(killed) < nkeep:
+                rand = random.choice(range(len(killed)))
+                rand = MPI.COMM_WORLD.bcast(rand, root=0)
+                killed.pop(rand)
+                check = True
+            if check:  # Make sure each core is killing the same individuals
+                all_killed = MPI.COMM_WORLD.allgather(tuple(id for id in killed))
+                assert len(set(all_killed)) <= 1
 
+            new_population = [individual for individual in population if individual.id not in killed]
             killed = [population[id] for id in killed]
-            new_population = [population[id] for id in to_keep]
             population.replace(new_population)
             return killed
         else:
             return []
 
+    @staticmethod
     @parallel
-    def get_equivalent_pairs(self, population):
-        """Get equivalent pairs in parallel
+    def get_equivalent_pairs(population, fingerprinter, fingerprinter_kwargs):
+        """Returns pairs of individuals that are equivalent.
 
         Args:
             population (Population): the population
         """
-        kwargs = self.function_kwargs[self.selected_fingerprinter]
         rank = gparameters.mpi.rank
         ncores = gparameters.mpi.ncores
         pairs_per_core = {r: [] for r in range(ncores)}
         for i, pair in enumerate(combinations(population, 2)):
             pairs_per_core[i % ncores].append(pair)
-        
-        count = []
-        equivalent_pairs = []
-        equivalent_pairs_per_core = []
-        for pair in pairs_per_core[rank]: 
-            are_the_same = self.selected_fingerprinter(*pair, **kwargs)
-            if are_the_same:
-                equivalent_pairs_per_core.append(pair)
-        print("Found {} equivalent pairs on rank {}".format(len(equivalent_pairs_per_core), rank))
-        count = MPI.COMM_WORLD.allgather(len(equivalent_pairs_per_core))
-        equivalent_pairs = MPI.COMM_WORLD.allgather(equivalent_pairs_per_core)
-        equivalent_pairs = [pair for pairs in equivalent_pairs for pair in pairs]
-        assert sum(count) == len(equivalent_pairs)
 
-        return equivalent_pairs
+        equivalent_pairs_by_core = []
+        for pair in pairs_per_core[rank]: 
+            are_the_same = fingerprinter(*pair, **fingerprinter_kwargs)
+            if are_the_same:
+                equivalent_pairs_by_core.append(pair)
+        if len(equivalent_pairs_by_core) > 0:
+            print("Found {} equivalent pairs on rank {}".format(len(equivalent_pairs_by_core), rank))
+        count = MPI.COMM_WORLD.allgather(len(equivalent_pairs_by_core))
+        all_equivalent_pairs = MPI.COMM_WORLD.allgather(equivalent_pairs_by_core)
+        all_equivalent_pairs = [pair for pairs in all_equivalent_pairs for pair in pairs]
+        assert sum(count) == len(all_equivalent_pairs)
+
+        return all_equivalent_pairs
 
     @single_core
     def post_processing(self):
